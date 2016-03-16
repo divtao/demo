c3b4178f363b37de6b1c3dfd522be9e119f5d811
divtao <guohaitao90@126.com>
2016-03-16 12:22:48 +0800

    设置Windows的大小

--- demo/AppDelegate.m
+++ demo/AppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by qianfeng on 16/3/16.
 //  Copyright © 2016年 HT. All rights reserved.
 //
-
+#import "ViewController.h"
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
