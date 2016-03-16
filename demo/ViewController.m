a4d6ddf762910831e387bc0da89bc4792f2dc984
divtao <guohaitao90@126.com>
2016-03-16 12:42:11 +0800

    button

--- demo/ViewController.m
+++ demo/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor  = [UIColor redColor];
+    UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:button];
 }
 
 - (void)didReceiveMemoryWarning {
